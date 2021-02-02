ab61d6faee099f1bf0a223c6438a086533098550
glopezl <78405967+glopezl@users.noreply.github.com>
2021-02-02 01:45:27 -0600

    Add files via upload

---
+++ Connect Powershell Client.ps1
@@ -0,0 +1,16 @@
+<# Team Workspace #>
+<# Servicios Básicos #>
+
+$credential = Get-Credential
+Connect-AzureAD -Credential $credential
+Connect-MsolService -Credential $credential 
+
+<#En caso de ser necesario el modulo ExchangeOnline correr el siguiente comando#> 
+
+<#Import-Module ExchangeOnlineManagement
+Connect-ExchangeOnline -Credential $credential -ShowProgress $true#>
+<#Out-GridView nos sirve para abrir una nueva ventana con los resultados Get#>
+
+<#Ejemplo de como exportar a csv#>
+<#Get-MsolUser | Select-Object UserPrincipalName, DisplayName, isLicensed | Export-Csv -Path C:\Powershell\test6.csv -Encoding UTF8#>
+
